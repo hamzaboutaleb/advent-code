7960c7ea5bbe7ab505a24a4acf56051e7ac6091a
hboutale <boutaleb.hm@gmail.com>
2025-07-20 18:18:42 +0100

    2015-day1

---
+++ 2015/day1/main.js
@@ -0,0 +1,41 @@
+// import { readFile } from "fs/promises";
+const fs = require('fs');
+
+const UP = "(";
+const DOWN = ")";
+
+function get_floor(input) {
+  let result = 0;
+  for (let i = 0; i < input.length; i++) {
+    const c = input[i];
+    if (c == UP) result++;
+    else if (c == DOWN) result--;
+  }
+  return result;
+}
+
+function get_basement_floor_id(input) {
+  let result = 0;
+  for (let i = 0; i < input.length; i++) {
+    const c = input[i];
+    if (c == UP) result++;
+    else if (c == DOWN) result--;
+    if (result == -1) return i + 1
+  }
+  return 0;
+}
+
+function main() {
+  const filename = process.argv[2];
+  try {
+    const data = fs.readFileSync(filename, "utf-8");
+    const floor = get_floor(data);
+    const basement = get_basement_floor_id(data);
+
+    console.log(`floor: ${floor}`);
+    console.log(`the position of the character that causes Santa to first enter the basement ${basement}`)
+  } catch (error) {
+    console.error(error);
+  }
+}
+main();
